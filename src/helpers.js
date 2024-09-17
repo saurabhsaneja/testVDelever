e9b6e128f95c5b9ac18bd5223983bfc6855dc64b
Saurabh Saneja <saurabh250183@gmail.com>
2024-09-17 00:43:28 -0700

    working on home screen

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,31 @@
 export const getSFProDisplayFont = (type) => {
-    return 'SF-Pro-Display-' + type
+    let fullType = ''
+    if (type === 'S') {
+        fullType = 'Semibold'
+    }
+    else if (type === 'BL') {
+        fullType = 'Black'
+    }
+    else if (type === 'BO') {
+        fullType = 'Black'
+    }
+    else if (type === 'H') {
+        fullType = 'Heavy'
+    }
+    else if (type === 'L') {
+        fullType = 'Light'
+    }
+    else if (type === 'M') {
+        fullType = 'Medium'
+    }
+    else if (type === 'R') {
+        fullType = 'Regular'
+    }
+    else if (type === 'T') {
+        fullType = 'Thin'
+    }
+    else if (type === 'U') {
+        fullType = 'Ultralight'
+    }
+    return 'SF-Pro-Display-' + fullType
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -6,12 +6,38 @@ import { ScreenNames } from '../global/Index';
 import { getSFProDisplayFont } from '../helpers';
 import Button from '../components/Button';
 import MyTextInput from '../components/MyTextInput';
+import LinearGradient from 'react-native-linear-gradient';
+import MenuImage from "../assets/images/menu.svg";
+import StatusImage from "../assets/images/status.svg";
+import CirclesImage from "../assets/images/circles.svg";
 
 const Home = ({ navigation }) => {
+  const { height, width } = useWindowDimensions();
+
+  const Header = () => {
+    return (
+      <View style={styles.headerContainer}>
+        <MenuImage width={24} height={24} />
+        <StatusImage />
+      </View>
+    )
+  }
+  const Welcome = () => {
+    return (
+      <View style={[styles.welcomeContainer, { width: 296 / 390 * width, height: height * 202 / 1087, marginTop: -(height * 94 / 1087) }]}>
+        <CirclesImage style={styles.circles} />
+        <Text style={styles.welcomeText} >Welcome,</Text>
+        <Text style={styles.findYourText} >Find Your{'\n'}Dream Sector!</Text>
+      </View>
+    )
+  }
   //UI
   return (
     <ScrollView contentContainerStyle={styles.container} >
-      <Text style={styles.username}>Welcome</Text>
+      <LinearGradient colors={['#9766FF', '#F7F3FF']} style={[styles.headerGradient, { height: height * 186 / 1087 }]}>
+        <Header />
+      </LinearGradient>
+      <Welcome />
     </ScrollView>
   );
 };
@@ -21,11 +47,46 @@ export default Home;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
-    // alignItems: 'center'
   },
   mainView: {
     width: '90%',
     alignSelf: 'center'
   },
+  headerGradient: {
+    // 390 * 186 (1087 screen height)
+    // height: '100%' * 186 / 1087,
+    width: '100%',
+    borderBottomRightRadius: 75,
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+  },
+  welcomeContainer: {
+    backgroundColor: '#EDFFCE',
+    borderTopLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    borderTopRightRadius: 25,
+    paddingLeft: 20,
+    paddingVertical: 20,
+  },
+  circles: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+  },
+  welcomeText: {
+    fontSize: 18,
+    fontFamily: getSFProDisplayFont('R'),
+    color: 'black'
+  },
+  findYourText: {
+    fontSize: 24,
+    fontFamily: getSFProDisplayFont('S'),
+    color: 'black',
+    marginTop: -10
+  }
 })
